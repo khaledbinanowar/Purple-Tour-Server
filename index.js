03bce97930132012059a546684b077cb3a67f4c3
khaledbinanowar <khaledbinanowar@gmail.com>
2021-10-31 01:06:27 +0600

    Second commit

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-const uri = "mongodb+srv://purple-tour:wOiQNKLxAwAkcX7y@cluster0.bti5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bti5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
         const database = client.db('purple-tour');
-        const servicesCollection = database.collection('services');
+        const serviceCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
 
         //GET services API
         app.get('/services', async (req, res) => {
-            const cursor = servicesCollection.find({});
+            const cursor = serviceCollection.find({});
             let services;
 
             services = await cursor.toArray();
@@ -35,11 +35,16 @@ async function run() {
         app.post('/services', async (req, res) => {
             const service = req.body;
             console.log('hit the post api', service);
-
-            const result = await servicesCollection.insertOne(service);
-            console.log(result);
+            const result = await serviceCollection.insertOne(service);
             res.json(result)
         });
+
+        // Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result);
+        })
 
     }
     finally {
